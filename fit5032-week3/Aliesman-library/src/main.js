bcf71f2cfe9bceb8a69e37ffcea8da64075acdd8
alie0018 <alie0018@student.monash.edu>
2024-08-08 13:35:39 +1000

    Adding Week 3 Studio tasks
    - completing the lab setups
    - completing the lab task 1-6
    - modifying the breakpoints dynamic (activity 7)

---
+++ fit5032-week3/Aliesman-library/src/main.js
@@ -0,0 +1,8 @@
+// import './assets/main.css'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { createApp } from 'vue'
+import App from './MainApp.vue'
+
+createApp(App).mount('#app')
